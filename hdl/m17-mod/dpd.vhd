332c77b18ade1ca6b83a66c388c66a6a59e2a5d5
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-02-11 10:05:10 +0100

    Update dpd.vhd
    add saturation

--- hdl/m17-mod/dpd.vhd
+++ hdl/m17-mod/dpd.vhd
@@ -27,22 +27,30 @@ architecture magic of dpd is
 	signal i2 : std_logic_vector(47 downto 0) := (others => '0');
 	signal i3 : std_logic_vector(63 downto 0) := (others => '0');
 	signal imul : std_logic_vector(31 downto 0) := (others => '0');
+	signal iesum : std_logic_vector(17 downto 0) := (others => '0'); --bit extended sum (16->18)
 
 	signal q1 : std_logic_vector(31 downto 0) := (others => '0');
 	signal q2 : std_logic_vector(47 downto 0) := (others => '0');
 	signal q3 : std_logic_vector(63 downto 0) := (others => '0');
 	signal qmul : std_logic_vector(31 downto 0) := (others => '0');
+	signal qesum : std_logic_vector(17 downto 0) := (others => '0'); --bit extended sum (16->18)
 begin
 	--0x4000 is "+1.00"
 	imul <= std_logic_vector(signed(i_i) * signed(i_i));
 	i1 <= std_logic_vector(signed(i_i) * p1);
 	i2 <= std_logic_vector(signed(imul) * p2) when signed(i_i)>=0 else std_logic_vector(-signed(imul) * p2);
 	i3 <= std_logic_vector(signed(imul) * signed(i_i) * p3);
-	i_o <= std_logic_vector(signed(i1(29 downto 14)) + signed(i2(43 downto 28)) + signed(i3(57 downto 42)));
+	iesum <= std_logic_vector(signed(i1(29) & i1(29) & i1(29 downto 14)) + signed(i2(43) & i2(43) & i2(43 downto 28)) + signed(i3(57) & i3(57) & i3(57 downto 42)));
+	i_o <= x"8000" when (signed(iesum)<-32768) else
+		x"7FFF" when (signed(iesum)>32767) else
+		std_logic_vector(signed(i1(29 downto 14)) + signed(i2(43 downto 28)) + signed(i3(57 downto 42)));
 
 	qmul <= std_logic_vector(signed(q_i) * signed(q_i));
 	q1 <= std_logic_vector(signed(q_i) * p1);
 	q2 <= std_logic_vector(signed(qmul) * p2) when signed(q_i)>=0 else std_logic_vector(-signed(qmul) * p2);
 	q3 <= std_logic_vector(signed(qmul) * signed(q_i) * p3);
-	q_o <= std_logic_vector(signed(q1(29 downto 14)) + signed(q2(43 downto 28)) + signed(q3(57 downto 42)));
+	qesum <= std_logic_vector(signed(q1(29) & q1(29) & q1(29 downto 14)) + signed(q2(43) & q2(43) & q2(43 downto 28)) + signed(q3(57) & q3(57) & q3(57 downto 42)));
+	q_o <= x"8000" when (signed(qesum)<-32768) else
+		x"7FFF" when (signed(qesum)>32767) else
+		std_logic_vector(signed(q1(29 downto 14)) + signed(q2(43 downto 28)) + signed(q3(57 downto 42)));
 end magic;
