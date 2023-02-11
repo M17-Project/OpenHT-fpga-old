802821e57de857adc9540e877e063d1c508c7849
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-02-11 10:37:28 +0100

    Update dpd.vhd
    simplification

--- hdl/m17-mod/dpd.vhd
+++ hdl/m17-mod/dpd.vhd
@@ -12,6 +12,7 @@ use IEEE.numeric_std.all;
 entity dpd is
 	port(
 		-- p1*x + p2*|x|^2 + p3*x^3
+		-- 0x4000 is "+1.00"
 		p1 : in signed(15 downto 0);
 		p2 : in signed(15 downto 0);
 		p3 : in signed(15 downto 0);
@@ -35,7 +36,6 @@ architecture magic of dpd is
 	signal qmul : std_logic_vector(31 downto 0) := (others => '0');
 	signal qesum : std_logic_vector(17 downto 0) := (others => '0'); --bit extended sum (16->18)
 begin
-	--0x4000 is "+1.00"
 	imul <= std_logic_vector(signed(i_i) * signed(i_i));
 	i1 <= std_logic_vector(signed(i_i) * p1);
 	i2 <= std_logic_vector(signed(imul) * p2) when signed(i_i)>=0 else std_logic_vector(-signed(imul) * p2);
@@ -43,7 +43,7 @@ begin
 	iesum <= std_logic_vector(signed(i1(29) & i1(29) & i1(29 downto 14)) + signed(i2(43) & i2(43) & i2(43 downto 28)) + signed(i3(57) & i3(57) & i3(57 downto 42)));
 	i_o <= x"8000" when (signed(iesum)<-32768) else
 		x"7FFF" when (signed(iesum)>32767) else
-		std_logic_vector(signed(i1(29 downto 14)) + signed(i2(43 downto 28)) + signed(i3(57 downto 42)));
+		iesum(15 downto 0);
 
 	qmul <= std_logic_vector(signed(q_i) * signed(q_i));
 	q1 <= std_logic_vector(signed(q_i) * p1);
@@ -52,5 +52,5 @@ begin
 	qesum <= std_logic_vector(signed(q1(29) & q1(29) & q1(29 downto 14)) + signed(q2(43) & q2(43) & q2(43 downto 28)) + signed(q3(57) & q3(57) & q3(57 downto 42)));
 	q_o <= x"8000" when (signed(qesum)<-32768) else
 		x"7FFF" when (signed(qesum)>32767) else
-		std_logic_vector(signed(q1(29 downto 14)) + signed(q2(43 downto 28)) + signed(q3(57 downto 42)));
+		qesum(15 downto 0);
 end magic;
