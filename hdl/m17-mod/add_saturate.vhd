539e9d0ede69cb7bfd7cfacb2178501250b35c1a
Wojciech Kaczmarski <44336093+sp5wwp@users.noreply.github.com>
2023-02-11 10:27:23 +0100

    Update add_saturate.vhd

--- hdl/m17-mod/add_saturate.vhd
+++ hdl/m17-mod/add_saturate.vhd
@@ -14,7 +14,7 @@ end add_saturate;
 architecture magic of add_saturate is
 	signal ext_sum : std_logic_vector(16 downto 0) := (others => '0'); -- bit extended sum
 begin
-	ext_sum <= std_logic_vector(signed(a_i(15) & a_i(15) & a_i(14 downto 0)) + signed(b_i(15) & b_i(15) & b_i(14 downto 0)));
+	ext_sum <= std_logic_vector(signed(a_i(15) & a_i(15 downto 0)) + signed(b_i(15) & b_i(15 downto 0)));
 	s_o <= x"8000" when (signed(ext_sum)<-32768) else
 		x"7FFF" when (signed(ext_sum)>32767) else
 		ext_sum(16) & ext_sum(14 downto 0);
